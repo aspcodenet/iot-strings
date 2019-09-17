8ece856260a934d41d7d59cc9356b7782a977915
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-17 11:09:42 +0200

    Add project files.

---
+++ IotStrings/IotStrings.py
@@ -0,0 +1,26 @@
+#text = input("Skriv in meddelande")
+
+#if text.find("nedrans") == -1:
+#    print("Bra inga fula ord")
+#else:
+#    print("Gå och tvätta munnen")
+#    text = text.replace("nedrans", "******")
+#    print(text)
+
+##text = text.upper()
+#if text.upper() == "J":
+#    print("exit")
+
+text = "12,hejsan,hoppsan";
+listaOfSubStrings = text.split(',')
+for sub in listaOfSubStrings:
+    print(sub)
+
+lista = [1,2,3]
+print(lista[4])
+
+print(lista[0])
+
+namn ="Stefan"
+print(namn[4])
+
